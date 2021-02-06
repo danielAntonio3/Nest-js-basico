377c759f02cba5ccf6a90a6777ae0811fee49267
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2021-02-06 13:24:20 -0600

    creando interfaces, modules y services

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+//* INTERFACES
+import { Task } from './interfaces/Task';
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Test',
+      description: 'Testing description',
+      done: true,
+    },
+  ];
+}
