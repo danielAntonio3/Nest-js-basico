3bc9181b319b936571b35d30fc776bd2d6db0373
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2021-02-06 14:59:01 -0600

    instalando mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
 import { TasksModule } from './tasks/tasks.module';
-
+// !IMPORTAMOS MONGODB
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule,MongooseModule.forRoot('mongodb://localhost/dbnest')],
   controllers: [AppController, TasksController],
   providers: [AppService, TasksService],
 })
